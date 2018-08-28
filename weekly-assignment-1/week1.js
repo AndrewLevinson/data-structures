6a4ca3e57753e1c109b80d3b4478fc76cafe5024
andrew_l <andrewlevins@gmail.com>
2018-08-28 01:25:09 -0400

    loop through array of page numbers to generate all 10 text files

--- weekly-assignment-1/week1.js
+++ weekly-assignment-1/week1.js
@@ -4,10 +4,23 @@
 var request = require("request");
 var fs = require("fs");
 
-request("https://parsons.nyc/thesis-2018/", function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    fs.writeFileSync("data/thesis.txt", body);
-  } else {
-    console.log("Request failed!");
+let pageNumber = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"];
+
+for (i = 0; i < pageNumber.length; i++) {
+  try {
+    throw i;
+  } catch (i) {
+    request("https://parsons.nyc/aa/m" + pageNumber[i] + ".html", function(
+      error,
+      response,
+      body
+    ) {
+      if (!error && response.statusCode == 200) {
+        fs.writeFileSync("data/" + pageNumber[i] + ".txt", body);
+        console.log(pageNumber[i]);
+      } else {
+        console.log("Request failed!");
+      }
+    });
   }
-});
+}
